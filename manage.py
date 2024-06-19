1b4ed7448c0911b470c6d7daaa54f24359ff6679
sax <s.apostolico@gmail.com>
2024-06-19 05:38:44 +0200

    add missing files

---
+++ manage.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import os
+import sys
+
+SRC = os.path.abspath("src")
+sys.path.insert(0, SRC)
+
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hope_dedup_engine.config.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
