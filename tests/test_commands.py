b29012fe90bc70db92b9e27b3f7b692eb7c80914
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2025-02-18 12:28:11 -0300

    chg ! by default command 'upgrade' do not run sync with HOPE

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -75,7 +75,6 @@ def test_upgrade_init(
             stdout=out,
             checks=False,
             verbosity=verbosity,
-            sync_with_hope=False,
         )
     assert "error" not in str(out.getvalue())
 
