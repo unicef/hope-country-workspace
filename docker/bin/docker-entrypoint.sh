e51583d24c27650c4cd5ff562970bbd8472c3618
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2025-04-22 07:19:51 -0400

    fix ! drop use gosu (CVE-2024-24790, CVE-2023-24540, CVE-2023-24538, CVE-2025-22871)

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -3,96 +3,47 @@
 
 export UWSGI_PROCESSES="${UWSGI_PROCESSES:-"4"}"
 export DJANGO_SETTINGS_MODULE="country_workspace.config.settings"
-mkdir -p "${MEDIA_ROOT}" "${STATIC_ROOT}" || echo "Cannot create dirs ${MEDIA_ROOT} ${STATIC_ROOT}"
 
-if [ -d "${MEDIA_ROOT}" ];then
-  chown -R hope:unicef ${MEDIA_ROOT}
-fi
-
-if [ -d "${STATIC_ROOT}" ];then
-  chown -R hope:unicef ${STATIC_ROOT}
-fi
-
-mkdir -p /app/
-chown -R hope:unicef /app
-cd /app
 
 case "$1" in
     run)
-      django-admin upgrade --with-check
-	    set -- tini -- "$@"
-	    MAPPING=""
-	    if [ "${STATIC_URL}" = "/static/" ]; then
-	      MAPPING="--static-map ${STATIC_URL}=${STATIC_ROOT}"
-	    fi
-      set -- tini -- "$@"
-	    set -- uwsgi --http :8000 \
-	          -H /venv \
-	          --module country_workspace.config.wsgi \
-	          --mimefile=/conf/mime.types \
-	          --uid hope \
-	          --gid unicef \
+        django-admin upgrade --with-check
+        set -- tini -- "$@"
+        MAPPING=""
+        if [ "${STATIC_URL}" = "/static/" ]; then
+            MAPPING="--static-map ${STATIC_URL}=${STATIC_ROOT}"
+        fi
+        set -- uwsgi --http :8000 \
+            -H /venv \
+            --module country_workspace.config.wsgi \
+            --mimefile=/conf/mime.types \
+            --uid hope \
+            --gid unicef \
             --buffer-size 8192 \
             --http-buffer-size 8192 \
-	          $MAPPING
-	    ;;
+            $MAPPING
+        ;;
     upgrade)
-      django-admin upgrade --with-check
-      ;;
+        set -- tini -- django-admin upgrade --with-check
+        ;;
     worker)
-      set -- tini -- "$@"
-      set -- gosu hope:unicef celery -A country_workspace.config.celery worker --statedb worker -E --loglevel=ERROR
-      ;;
+        set -- tini -- celery -A country_workspace.config.celery worker --statedb worker -E --loglevel=ERROR
+        ;;
     beat)
-      set -- tini -- "$@"
-      set -- gosu hope:unicef celery -A country_workspace.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler
-      ;;
+        set -- tini -- celery -A country_workspace.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler
+        ;;
     flower)
-      export DATABASE_URL="sqlite://:memory:"
-      set -- tini -- "$@"
-      set -- gosu hope:unicef celery -A country_workspace.config.celery flower
-      ;;
+        if [ -z "$DATABASE_URL" ]; then
+            echo "Warning: DATABASE_URL is not set, defaulting to in-memory SQLite for Flower"
+            export DATABASE_URL="sqlite://:memory:"
+        fi
+        set -- tini -- celery -A country_workspace.config.celery flower
+        ;;
+    *)
+        echo "Unknown command: $1"
+        echo "Supported commands: run, upgrade, worker, beat, flower"
+        exit 1
+        ;;
 esac
 
 exec "$@"
-
-
-#
-#case "$1" in
-#    run)
-#      if [ "$INIT_RUN_CHECK" = "1" ];then
-#        echo "Running Django checks..."
-#        django-admin check --deploy
-#      fi
-#      OPTS="--no-check -v 1"
-#      if [ "$INIT_RUN_UPGRADE" = "1" ];then
-#        if [ "$INIT_RUN_COLLECTSTATIC" != "1" ];then
-#          OPTS="$OPTS --no-static"
-#        fi
-#        if [ "$INIT_RUN_MIGRATATIONS" != "1" ];then
-#          OPTS="$OPTS --no-migrate"
-#        fi
-#        echo "Running 'upgrade $OPTS'"
-#        django-admin upgrade $OPTS
-#      fi
-#      set -- tini -- "$@"
-#      echo "Starting uwsgi..."
-#      exec uwsgi --ini /conf/uwsgi.ini
-#      ;;
-#    worker)
-#      exec celery -A country_workspace.celery worker -E --loglevel=ERROR --concurrency=4
-#      ;;
-#    beat)
-#      exec celery -A country_workspace.celery beat -E --loglevel=ERROR ---scheduler django_celery_beat.schedulers:DatabaseScheduler
-#      ;;
-#    dev)
-#      until pg_isready -h db -p 5432;
-#        do echo "waiting for database"; sleep 2; done;
-#      django-admin collectstatic --no-input
-#      django-admin migrate
-#      django-admin runserver 0.0.0.0:8000
-#      ;;
-#    *)
-#      exec "$@"
-#      ;;
-#esac
