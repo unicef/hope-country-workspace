8b15da3c7debdcba70ab0de3915b2dabc0bd292c
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2025-04-29 05:23:48 -0400

    chg ! upgrade packages (CVE-2025-43859)

--- docker/bin/docker-entrypoint.sh
+++ docker/bin/docker-entrypoint.sh
@@ -8,12 +8,11 @@ export DJANGO_SETTINGS_MODULE="country_workspace.config.settings"
 case "$1" in
     run)
         django-admin upgrade --with-check
-        set -- tini -- "$@"
         MAPPING=""
         if [ "${STATIC_URL}" = "/static/" ]; then
             MAPPING="--static-map ${STATIC_URL}=${STATIC_ROOT}"
         fi
-        set -- uwsgi --http :8000 \
+        set -- tini -- uwsgi --http :8000 \
             -H /venv \
             --module country_workspace.config.wsgi \
             --mimefile=/conf/mime.types \
@@ -21,23 +20,23 @@ case "$1" in
             --gid unicef \
             --buffer-size 8192 \
             --http-buffer-size 8192 \
-            $MAPPING
+            $MAPPING "$@"
         ;;
     upgrade)
-        set -- tini -- django-admin upgrade --with-check
+        set -- tini -- django-admin upgrade --with-check "$@"
         ;;
     worker)
-        set -- tini -- celery -A country_workspace.config.celery worker --statedb worker -E --loglevel=ERROR
+        set -- tini -- celery -A country_workspace.config.celery worker --statedb worker -E --loglevel=ERROR "$@"
         ;;
     beat)
-        set -- tini -- celery -A country_workspace.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler
+        set -- tini -- celery -A country_workspace.config.celery beat --loglevel=ERROR --scheduler django_celery_beat.schedulers:DatabaseScheduler "$@"
         ;;
     flower)
         if [ -z "$DATABASE_URL" ]; then
             echo "Warning: DATABASE_URL is not set, defaulting to in-memory SQLite for Flower"
             export DATABASE_URL="sqlite://:memory:"
         fi
-        set -- tini -- celery -A country_workspace.config.celery flower
+        set -- tini -- celery -A country_workspace.config.celery flower "$@"
         ;;
     *)
         echo "Unknown command: $1"
