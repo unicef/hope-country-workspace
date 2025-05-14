17d50459d060d9b9ab9c739f5848ff32bb80f48f
data <data.mikava@gmail.com>
2025-05-14 21:16:11 +0400

    Setup seleniumbase

--- tests/extras/testutils/selenium.py
+++ tests/extras/testutils/selenium.py
@@ -1,52 +1,69 @@
-from django.urls import reverse
+from seleniumbase import BaseCase
 
 
-def check_link_by_class(selenium, cls, view_name):
-    link = selenium.find_element_by_class_name(cls)
-    url = reverse(f"{view_name}")
-    return f' href="{url}"' in link.get_attribute("innerHTML")
+class CountryWorkspaceSeleniumTC(BaseCase):
+    live_server_url: str = ""
 
+    def setUp(self, masterqa_mode=False):
+        super().setUp()
+        from testutils.factories.user import SuperUserFactory
 
-def wait_for(driver, *args):
-    from selenium.webdriver.support import expected_conditions as ec
-    from selenium.webdriver.support.ui import WebDriverWait
+        super().setUpClass()
+        self.admin_user = SuperUserFactory()
+        self.admin_user._password = "password"
 
-    wait = WebDriverWait(driver, 10)
-    wait.until(ec.visibility_of_element_located((*args,)))
-    return driver.find_element(*args)
+    def tearDown(self):
+        self.save_teardown_screenshot()
+        super().tearDown()
+        self.admin_user.delete()
 
+    def base_method(self):
+        pass
 
-def wait_for_url(driver, url):
-    from selenium.webdriver.support import expected_conditions as ec
-    from selenium.webdriver.support.ui import WebDriverWait
+    def open(self, url: str):
+        self.maximize_window()
+        return super().open(f"{self.live_server_url}{url}")
 
-    if "://" not in url:
-        url = f"{driver.live_server.url}{url}"
-    wait = WebDriverWait(driver, 10)
-    wait.until(ec.url_contains(url))
+    def select2_select(self, element_id: str, value: str):
+        self.slow_click(f"span[aria-labelledby=select2-{element_id}-container]")
+        self.wait_for_element_visible("input.select2-search__field")
+        self.click(f"li.select2-results__option:contains('{value}')")
+        self.wait_for_element_absent("input.select2-search__field")
 
+    def login_as_user(self, user=None):
+        if user is not None:
+            self.admin_user = user
+        self.open("/login/")
+        self.type("input[name=username]", f"{self.admin_user.username}")
+        self.type("input[name=password]", f"{self.admin_user._password}")
+        self.submit('input[value="Login"]')
+        self.wait_for_ready_state_complete()
 
-def force_login(user, driver, base_url):
-    from importlib import import_module
+    def login(self, url=None):
+        self.open("/admin/")
+        if self.get_current_url() == f"{self.live_server_url}/admin/login/?next=/admin/":
+            self.type("input[name=username]", f"{self.admin_user.username}")
+            self.type("input[name=password]", f"{self.admin_user._password}")
+            self.submit('input[value="Log in"]')
+            self.wait_for_ready_state_complete()
 
-    from django.conf import settings
-    from django.contrib.auth import BACKEND_SESSION_KEY, HASH_SESSION_KEY, SESSION_KEY
+    def is_required(self, element: str) -> bool:
+        el = self.wait_for_element_visible(element)
+        return el.parent.find_element("label>span").text == "(required)"
 
-    SessionStore = import_module(settings.SESSION_ENGINE).SessionStore  # noqa: N806
-    with driver.with_timeouts(page=5):
-        driver.get(base_url)
+    def get_field_error(self, element: str) -> bool:
+        return self.wait_for_element_visible(f"fieldset.{element} ul.errorlist").text
 
-    session = SessionStore()
-    session[SESSION_KEY] = user._meta.pk.value_to_string(user)
-    session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-    session[HASH_SESSION_KEY] = user.get_session_auth_hash()
-    session.save()
-
-    driver.add_cookie(
-        {
-            "name": settings.SESSION_COOKIE_NAME,
-            "value": session.session_key,
-            "path": "/",
-        }
-    )
-    driver.refresh()
+    def get_pixel_colors(self):
+        # Return the RGB colors of the canvas element's top left pixel
+        x = 0
+        y = 0
+        if self.browser == "safari":
+            x = 1
+            y = 1
+        color = self.execute_script(
+            "return document.querySelector('canvas').getContext('2d').getImageData(%s,%s,1,1).data;" % (x, y)
+        )
+        if self.is_chromium():
+            return [color[0], color[1], color[2]]
+        return [color["0"], color["1"], color["2"]]
