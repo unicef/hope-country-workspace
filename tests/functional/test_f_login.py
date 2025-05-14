832da60f64246a1e8b3f4722ef639ca0eacbbe7e
data <data.mikava@gmail.com>
2025-05-14 19:16:56 +0400

    fix admin login text

--- tests/functional/test_f_login.py
+++ tests/functional/test_f_login.py
@@ -5,5 +5,5 @@ pytestmark = pytest.mark.xdist_group("selenium")
 
 @pytest.mark.selenium
 def test_login(browser):
-    # Not sure if this makes any sense
     browser.login()
+    browser.assert_text("Welcome to HOPE Workspace", "div#content h1")
