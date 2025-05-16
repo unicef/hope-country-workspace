10da05c9d2f803f050707d09f387cb494b57eee3
Sergey Misuk <sergey.misuk@gmail.com>
2025-05-16 12:48:57 +0300

    Add consent_sharing field

---
+++ src/country_workspace/versioning/scripts/0007_consent_sharing.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from hope_flex_fields.models import Fieldset, FieldDefinition, FlexField
+from packaging.version import Version
+
+from country_workspace.contrib.hope.constants import HOUSEHOLD_CHECKER_NAME
+
+_script_for_version = Version("0.1.0")
+
+CONSENT_SHARING = "consent_sharing"
+
+
+def forward() -> None:
+    household_fieldset = Fieldset.objects.get(name=HOUSEHOLD_CHECKER_NAME)
+    household_fieldset.fields.create(
+        name=CONSENT_SHARING,
+        definition=FieldDefinition.objects.get(field_type=forms.CharField),
+        attrs={},
+    )
+
+
+def backward() -> None:
+    FlexField.objects.get(name=CONSENT_SHARING).delete()
+
+
+class Scripts:
+    requires = []
+    operations = [(forward, backward)]
