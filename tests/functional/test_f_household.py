059a09488589b9cbaefcededb6c8e03eaaa19beb
data <data.mikava@gmail.com>
2025-05-21 19:51:36 +0400

    Refactor household selenium tests

--- tests/functional/test_f_household.py
+++ tests/functional/test_f_household.py
@@ -2,6 +2,9 @@ from typing import TYPE_CHECKING
 
 import pytest
 from django.core.management import call_command
+from testutils.factories import OfficeFactory
+from testutils.factories.program import BeneficiaryGroupFactory
+from testutils.factories import CountryProgramFactory
 
 if TYPE_CHECKING:
     from country_workspace.workspaces.models import CountryHousehold
@@ -16,22 +19,20 @@ def create_checkers() -> None:
 
 @pytest.fixture
 def office(db, worker_id):
-    from testutils.factories import OfficeFactory
-
     return OfficeFactory()
 
 
 @pytest.fixture
-def program(office, household_checker, individual_checker):
-    from testutils.factories import CountryProgramFactory
-    from testutils.factories.program import BeneficiaryGroupFactory
-
-    beneficiary_group = BeneficiaryGroupFactory(
+def beneficiary_group():
+    return BeneficiaryGroupFactory(
         group_label_plural="Households",
         member_label_plural="Individuals",
         master_detail=True,
     )
 
+
+@pytest.fixture
+def program(office, household_checker, individual_checker, beneficiary_group):
     return CountryProgramFactory(
         country_office=office,
         household_columns="name\nid\n",
@@ -49,44 +50,22 @@ def household(program):
     return CountryHouseholdFactory(batch__program=program, batch__country_office=program.country_office)
 
 
-@pytest.fixture
-def individual(household):
-    from testutils.factories import IndividualFactory
+@pytest.mark.selenium
+def test_list_household(browser, household: "CountryHousehold"):
+    browser.login_as_user()
+    browser.select_option_by_text("select[name=tenant]", household.program.country_office.name)
+    browser.select2_select("id_program", household.program.name)
 
-    IndividualFactory(batch=household.batch, household=household)
-    IndividualFactory(batch=household.batch, household=household)
-    household.flex_fields["size"] += 2
+    browser.click_link("Households")
+    browser.click_link(str(household.name))
+    browser.assert_url(f"{browser.live_server_url}{household.get_change_url()}")
+
+    browser.click("a.closelink")
+    browser.assert_url(f"{browser.live_server_url}/workspaces/countryhousehold/")
 
 
 @pytest.mark.selenium
-def test_list_household(browser, admin_user, household: "CountryHousehold"):
-    from testutils.perms import user_grant_permissions
-
-    with user_grant_permissions(
-        admin_user,
-        [
-            "workspaces.view_countryhousehold",
-            "workspaces.view_countryindividual",
-            "workspaces.view_countryprogram",
-        ],
-        household.program.country_office,
-    ):
-        browser.login_as_user()
-        # Select Tenant
-        browser.select_option_by_text("select[name=tenant]", household.program.country_office.name)
-        browser.select2_select("id_program", household.program.name)
-
-        browser.click_link("Households")
-        browser.click_link(str(household.name))
-        browser.assert_url(f"{browser.live_server_url}{household.get_change_url()}")
-
-        browser.click("a.closelink")
-        browser.assert_url(f"{browser.live_server_url}/workspaces/countryhousehold/")
-
-
-@pytest.mark.selenium
-def test_list_household_select_all_fields(browser, admin_user, household: "CountryHousehold"):
-    # Testing workspaces/static/js/select-all.js functionality
+def test_list_household_select_all_fields(browser, household: "CountryHousehold"):
     browser.login_as_user()
     browser.select_option_by_text("select[name=tenant]", household.program.country_office.name)
     browser.select2_select("id_program", household.program.name)
