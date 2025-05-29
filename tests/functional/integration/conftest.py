a35559cbd3b0c2eaed2f7acfae2fd39424c535a7
data <data.mikava@gmail.com>
2025-05-29 14:34:19 +0400

    add tests for invalid data. add tests for push people

--- tests/functional/integration/conftest.py
+++ tests/functional/integration/conftest.py
@@ -4,6 +4,9 @@ from country_workspace.models import SyncLog
 from testutils.factories import OfficeFactory
 
 
+HOPE_PROGRAM_ID = "a4cad4c6-b512-42a5-9be5-cce9760a46d8"
+
+
 @pytest.fixture(autouse=True)
 def sync_checkers(prepare_checkers) -> None:
     SyncLog.objects.refresh()
@@ -16,3 +19,8 @@ def office():
         slug="afghanistan",
         code="AF",
     )
+
+
+@pytest.fixture
+def program_id():
+    return HOPE_PROGRAM_ID
--- tests/functional/integration/test_hope_push_people.py
+++ tests/functional/integration/test_hope_push_people.py
@@ -1,17 +1,18 @@
-import sys
-import uuid
+import time
 from random import randint, choice
 from typing import TYPE_CHECKING
 
 import pytest
 from django_celery_results.models import TaskResult
+from faker import Faker
 
 from country_workspace.models import AsyncJob
-from testutils.factories import CountryHouseholdFactory
+from testutils.factories import IndividualFactory, CountryBatchFactory
 from testutils.factories.program import BeneficiaryGroupFactory, CountryProgramFactory
 
+
 if TYPE_CHECKING:
-    from country_workspace.workspaces.models import CountryHousehold
+    from country_workspace.workspaces.models import CountryIndividual, CountryProgram
 
 
 @pytest.fixture
@@ -19,7 +20,7 @@ def beneficiary_group():
     return BeneficiaryGroupFactory(
         group_label_plural="Households",
         member_label_plural="Individuals",
-        master_detail=True,
+        master_detail=False,
     )
 
 
@@ -37,51 +38,49 @@ def program(office, program_id, household_checker, individual_checker, beneficia
 
 
 @pytest.fixture
-def household(program):
-    household = CountryHouseholdFactory(
-        batch__program=program,
-        batch__country_office=program.country_office,
-        flex_fields={
-            "country": program.country_office.code,
-            "collect_individual_data": True,
-            "consent": True,
-            "household_id": randint(1, sys.maxsize),
-            "size": randint(3, 6),
-        },
+def batch(program):
+    return CountryBatchFactory(
+        program=program,
+        country_office=program.country_office,
     )
 
-    first_member = True
-    for member in household.members.all():
-        member.flex_fields = {
-            "birth_date": member.flex_fields["birth_date"],
-            "full_name": member.flex_fields["full_name"],
-            "gender": member.flex_fields["gender"],
-            "relationship": "HEAD" if first_member else choice(["SON_DAUGHTER", "BROTHER_SISTER", "FOSTER_CHILD"]),
-            "primary_collector_id": household.flex_fields["household_id"],
-        }
-        if first_member:
-            member.flex_fields["role"] = "PRIMARY"
-        member.save()
-        first_member = False
 
-    return household
+@pytest.fixture
+def individuals(program, batch):
+    fake = Faker()
+    individuals = []
+    for _i in range(randint(1, 10)):
+        individual = IndividualFactory(
+            household=None,
+            batch=batch,
+            flex_fields={
+                "birth_date": fake.date_between(start_date="-40y", end_date="-10y").strftime("%Y-%m-%d"),
+                "full_name": f"{fake.first_name()} {fake.last_name()}",
+                "gender": choice(["MALE", "FEMALE"]),
+                "country": fake.country_code(),
+                "type": choice(["", "NON_BENEFICIARY"]),
+                "residence_status": choice(["", "IDP", "REFUGEE", "OTHERS_OF_CONCERN", "HOST", "RETURNEE"]),
+                "relationship": choice(["HEAD", "SON_DAUGHTER", "BROTHER_SISTER", "FOSTER_CHILD"]),
+            },
+        )
+        individuals.append(individual)
+    return individuals
 
 
 @pytest.mark.integration
 @pytest.mark.django_db
-def test_hope_push_success(browser, household: "CountryHousehold"):
+def test_hope_push_success(browser, individuals: list["CountryIndividual"], program: "CountryProgram"):
     browser.login_as_user()
-    browser.select_option_by_text("select[name=tenant]", household.program.country_office.name)
-    browser.select2_select("id_program", household.program.name)
-    browser.click_link("Households")
-
+    browser.select_option_by_text("select[name=tenant]", program.country_office.name)
+    browser.select2_select("id_program", program.name)
+    browser.click_link("Individuals")
     browser.click("#action-toggle")
     browser.select_option_by_value("select[name=action]", "validate_records")
     browser.click('button[type="submit"][name="index"]')
     browser.click("#action-toggle")
     browser.select_option_by_value("select[name=action]", "push_to_hope")
     browser.click('button[type="submit"][name="index"]')
-    browser.type("input[name=batch_name]", f"integration_test_{uuid.uuid4()}")
+    browser.type("input[name=batch_name]", f"integration_test_{time.time()}")
     browser.click('input[type="submit"][name="_push"]')
 
     browser.assert_url(f"{browser.live_server_url}/workspaces/countryasyncjob/")
@@ -90,19 +89,19 @@ def test_hope_push_success(browser, household: "CountryHousehold"):
         action="country_workspace.contrib.hope.push.push_to_hope_core",
     )
     result_text = TaskResult.objects.values_list("result", flat=True).get(task_id=task_result_id)
-    assert result_text == '{"errors": [], "households": 1}'
+    assert result_text == f'{{"errors": [], "people": {len(individuals)}}}'
 
 
 @pytest.mark.integration
 @pytest.mark.django_db
-def test_hope_push_invalid(browser, household: "CountryHousehold"):
-    household.flex_fields["country"] = "WRONG_COUNTRY_CODE"
-    household.save()
+def test_hope_push_invalid(browser, individuals: list["CountryIndividual"], program: "CountryProgram"):
+    individuals[0].flex_fields = {}
+    individuals[0].save()
+    valid_ind_count = len(individuals) - 1
     browser.login_as_user()
-    browser.select_option_by_text("select[name=tenant]", household.program.country_office.name)
-    browser.select2_select("id_program", household.program.name)
-    browser.click_link("Households")
-
+    browser.select_option_by_text("select[name=tenant]", program.country_office.name)
+    browser.select2_select("id_program", program.name)
+    browser.click_link("Individuals")
     browser.click("#action-toggle")
     browser.select_option_by_value("select[name=action]", "validate_records")
     browser.click('button[type="submit"][name="index"]')
@@ -111,4 +110,4 @@ def test_hope_push_invalid(browser, household: "CountryHousehold"):
         action="country_workspace.workspaces.admin.cleaners.validate.validate_queryset",
     )
     result_text = TaskResult.objects.values_list("result", flat=True).get(task_id=task_result_id)
-    assert result_text == '{"valid": 0, "invalid": 1}'
+    assert result_text == f'{{"valid": {valid_ind_count}, "invalid": 1}}'
