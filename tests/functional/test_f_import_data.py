43db888b4c357cd414424a2352e56ea27df2f1bf
patsatsia <giorgi.patsatsia@valor-software.com>
2025-06-02 16:33:46 +0400

    Add test for aurora import tab with two different programs with different beneficiary groups

--- tests/functional/test_f_import_data.py
+++ tests/functional/test_f_import_data.py
@@ -1,5 +1,9 @@
 import pytest
 from django.core.management import call_command
+from testutils.factories import CountryProgramFactory
+from testutils.factories import OfficeFactory
+
+from tests.extras.testutils.factories.program import BeneficiaryGroupFactory
 
 
 @pytest.fixture(autouse=True)
@@ -8,10 +12,47 @@ def create_checkers() -> None:
 
 
 @pytest.fixture
+def office(db, worker_id):
+    return OfficeFactory()
+
+
+@pytest.fixture
+def program(office):
+    return CountryProgramFactory(country_office=office)
+
+
+@pytest.fixture
+def beneficiary_group():
+    return BeneficiaryGroupFactory(
+        group_label_plural="Households",
+        member_label_plural="Individuals",
+        master_detail=True,
+    )
+
+
+@pytest.fixture
+def program_beneficiary(office, beneficiary_group):
+    return CountryProgramFactory(
+        country_office=office,
+        beneficiary_group=beneficiary_group,
+    )
+
+
+@pytest.fixture
 def browser_program(browser, program):
     browser.login_as_user()
     browser.select_option_by_text("select[name=tenant]", program.country_office.name)
     browser.select2_select("id_program", program.name)
+    browser.click_link("Programme")
+    browser.click("#btn-import_data")
+    return browser
+
+
+@pytest.fixture
+def browser_program_beneficiary(browser, program_beneficiary):
+    browser.login_as_user()
+    browser.select_option_by_text("select[name=tenant]", program_beneficiary.country_office.name)
+    browser.select2_select("id_program", program_beneficiary.name)
     browser.click_link("Programme")
     browser.click("#btn-import_data")
     return browser
@@ -45,6 +86,28 @@ def test_aurora_import_tab(browser_program):
     aurora_input_ids = [
         "#id_aurora-batch_name",
         "#id_aurora-registration",
+        "#id_aurora-individuals_column_prefix",
+        "#id_aurora-check_before",
+        "#id_aurora-fail_if_alien",
+    ]
+
+    for input_id in aurora_input_ids:
+        browser_program.assert_element_visible(input_id)
+
+    assert browser_program.get_value("#id_aurora-individuals_column_prefix") == "individuals_"
+
+
+@pytest.mark.selenium
+def test_aurora_import_tab_with_beneficiary(browser_program_beneficiary):
+    browser_program_beneficiary.click('button[data-input-value="aurora"]')
+    browser_program_beneficiary.assert_element_not_visible("#tab-rdi")
+    browser_program_beneficiary.assert_element_visible("#tab-aurora")
+    browser_program_beneficiary.assert_element_not_visible("#tab-kobo")
+    browser_program_beneficiary.assert_element_present('button[data-input-value="aurora"].selected')
+
+    aurora_input_ids = [
+        "#id_aurora-batch_name",
+        "#id_aurora-registration",
         "#id_aurora-household_column_prefix",
         "#id_aurora-individuals_column_prefix",
         "#id_aurora-household_label_column",
@@ -53,11 +116,11 @@ def test_aurora_import_tab(browser_program):
     ]
 
     for input_id in aurora_input_ids:
-        browser_program.assert_element_visible(input_id)
+        browser_program_beneficiary.assert_element_visible(input_id)
 
-    assert browser_program.get_value("#id_aurora-household_column_prefix") == "household_"
-    assert browser_program.get_value("#id_aurora-individuals_column_prefix") == "individuals_"
-    assert browser_program.get_value("#id_aurora-household_label_column") == "family_name"
+    assert browser_program_beneficiary.get_value("#id_aurora-household_column_prefix") == "household_"
+    assert browser_program_beneficiary.get_value("#id_aurora-individuals_column_prefix") == "individuals_"
+    assert browser_program_beneficiary.get_value("#id_aurora-household_label_column") == "family_name"
 
 
 @pytest.mark.selenium
