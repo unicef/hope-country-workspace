710f765e80fc48c4415f52acd0e1211e04bf705b
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2025-06-20 08:40:35 -0400

    chg ! RDI for people

--- src/country_workspace/workspaces/admin/forms.py
+++ src/country_workspace/workspaces/admin/forms.py
@@ -4,6 +4,7 @@ from django import forms
 
 from country_workspace.workspaces.admin.cleaners.base import BaseActionForm
 from country_workspace.workspaces.validators import ValidatableFileValidator
+from country_workspace.models import BeneficiaryGroup
 
 if TYPE_CHECKING:
     from hope_flex_fields.models import DataChecker
@@ -54,6 +55,12 @@ class ImportFileForm(forms.Form):
         help_text="Which column should be used as label for the household. It can use interpolation",
     )
 
+    people_column_prefix = forms.CharField(
+        required=False,
+        initial="pp_",
+        help_text="People' column group prefix",
+    )
+
     first_line = forms.IntegerField(required=True, initial=0, help_text="First line to process")
 
     check_before = forms.BooleanField(
@@ -63,3 +70,14 @@ class ImportFileForm(forms.Form):
         required=False, help_text="Fails if it finds fields which do not exists in data checker."
     )
     file = forms.FileField(validators=[ValidatableFileValidator()])
+
+    def __init__(self, *args: Any, beneficiary_group: BeneficiaryGroup | None = None, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        if beneficiary_group:
+            exclude_fields = (
+                ("people_column_prefix",)
+                if beneficiary_group.master_detail
+                else ("pk_column_name", "master_column_label", "detail_column_label")
+            )
+            for field_name in exclude_fields:
+                self.fields.pop(field_name, None)
