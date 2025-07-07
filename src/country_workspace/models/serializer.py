7829af6e2f60bfcb20f5f918ac33f76d47c4724f
patsatsia <giorgi.patsatsia@valor-software.com>
2025-07-07 13:16:08 +0400

    Alter 'serialize' method
    Change type hints for 'data' and require list of dicts

--- src/country_workspace/models/serializer.py
+++ src/country_workspace/models/serializer.py
@@ -1,5 +1,7 @@
-from django.db import models
+from typing import Any
+
 from django.core.exceptions import ValidationError
+from django.db import models
 
 from country_workspace.models.base import BaseModel
 from country_workspace.utils.js_executor import JavaScriptExecutor
@@ -9,7 +11,7 @@ class DataSerializer(BaseModel):
     name = models.SlugField(max_length=64, unique=True)
     code = models.TextField()
 
-    def serialize(self, data: dict) -> dict:
+    def serialize(self, data: list[dict]) -> Any:
         executor = JavaScriptExecutor(data=data, code=self.code)
         return executor.execute()
 
@@ -17,8 +19,5 @@ class DataSerializer(BaseModel):
         return f"DataSerializer: {self.name}"
 
     def clean(self) -> None:
-        if not self.code:
-            raise ValidationError("Code is required")
-
         if not JavaScriptExecutor.is_valid_js(self.code):
             raise ValidationError("Invalid JavaScript code")
