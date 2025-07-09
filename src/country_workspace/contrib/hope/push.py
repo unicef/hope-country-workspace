e3ca0d89b1ee10f12114fef8c4a76a9e0df449f0
data <data.mikava@gmail.com>
2025-07-09 14:59:09 +0400

    Add flex fields grouping

--- src/country_workspace/contrib/hope/push.py
+++ src/country_workspace/contrib/hope/push.py
@@ -1,5 +1,6 @@
 from collections.abc import Callable, Iterator
 from dataclasses import dataclass, field
+from functools import cached_property
 from itertools import batched
 from json import JSONDecodeError
 from typing import Any, TypedDict, ReadOnly
@@ -12,7 +13,7 @@ from requests.exceptions import RequestException
 
 from country_workspace.contrib.hope.client import HopeClient
 from country_workspace.exceptions import RemoteError
-from country_workspace.models import AsyncJob, Rdp
+from country_workspace.models import AsyncJob, Rdp, Program
 from country_workspace.workspaces.models import CountryHousehold, CountryIndividual
 
 
@@ -188,18 +189,26 @@ class PushProcessor(BatchErrorHandlerMixin):
             self.total["errors"].append(f"{error_msg}: {e}")
             return None
 
+    @cached_property
+    def program(self) -> Program:
+        return Program.objects.get(hope_id=self.program_hope_id)
+
     def prepare_batch(self) -> tuple[list[int], list[dict]]:
         """Prepare a batch of household/individual|people data for API submission."""
         ids, data = [], []
+        filter_none = lambda d: {k: v for k, v in d.items() if v is not None}
         for item in self.queryset:
             ids.append(item.id)
-            filter_none = lambda d: {k: v for k, v in d.items() if v is not None}
-            data.append(
-                {**filter_none(item.flex_fields), "members": [filter_none(m.flex_fields) for m in item.members.all()]}
-                if self.master_detail
-                else filter_none(item.flex_fields)
-            )
-        return ids, data
+            flex_fields = item.apply_grouping()
+            if self.master_detail:
+                flex_fields["members"] = []
+                for member in item.members.all():
+                    flex_fields["members"].append(member.apply_grouping())
+                data.append(filter_none(flex_fields))
+            else:
+                data.append(filter_none(flex_fields))
+
+        return ids, self.program.serialize(data)
 
     def process_batch_response(self, response: dict | None, batch_ids: list[int]) -> list[int]:
         """Process the API response for a batch push operation."""
---
+++ src/country_workspace/models/mixins.py
@@ -0,0 +1,36 @@
+from collections import defaultdict
+
+
+class FlexFieldGroupingMixin:
+    def get_grouping_info(self) -> dict[str, list[str]]:
+        grouping_info = defaultdict(list)
+        for member in self.checker.members.select_related("fieldset").all():
+            if not member.prefix:
+                continue
+            if member.group == "":
+                continue
+            if member.group is None:
+                grouping_key = member.fieldset.group
+            else:
+                grouping_key = member.group
+
+            if grouping_key:
+                grouping_info[grouping_key].append(member.prefix)
+
+        return grouping_info
+
+    def apply_grouping(self) -> dict:
+        grouping_info = self.get_grouping_info()
+        grouped_data = {}
+        flex_fields = dict(self.flex_fields)
+        for grouping_key, prefixes in grouping_info.items():
+            grouped_data[grouping_key] = []
+            for prefix in prefixes:
+                _data = {}
+                for field_name in self.flex_fields:
+                    if field_name.startswith(prefix):
+                        _data[field_name.removeprefix(prefix)] = flex_fields.pop(field_name)
+                grouped_data[grouping_key].append(_data)
+        grouped_data.update(flex_fields)
+
+        return grouped_data
