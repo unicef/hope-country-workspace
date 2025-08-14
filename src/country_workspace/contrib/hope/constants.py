946f8282fde1f8c50fb3849f4f0fbd85865b2fc3
patsatsia <giorgi.patsatsia@valor-software.com>
2025-08-14 17:15:08 +0400

    Update constants according to latest requirements

--- src/country_workspace/contrib/hope/constants.py
+++ src/country_workspace/contrib/hope/constants.py
@@ -14,6 +14,40 @@ ADMINAREAS_FIELDSET_NAME: Final[str] = "HOPE Admin Areas"
 ACCOUNT_FIELDSET_NAME: Final[str] = "HOPE Account"
 DOCUMENT_FIELDSET_NAME: Final[str] = "HOPE Document"
 
-DOCUMENT_TYPES = ("national_id", "national_passport")
+# Updated document types to match HOPE Core API
+DOCUMENT_TYPES = ("national_id", "national_passport", "birth_certificate", "other")
 
-ACCOUNT_TYPES = ("mobile", "bank")
+# Updated account types to match HOPE Core API
+ACCOUNT_TYPES = ("mobile", "bank", "cash", "other")
+
+# Field mappings for HOPE Core API transformation
+INDIVIDUAL_FIELD_MAPPINGS = {
+    "given_name": "first_name",
+    "family_name": "last_name",
+    "birth_date": "birth_date",
+    "marital_status": "marital_status",
+    "observed_disability": "observed_disability",
+    "relationship": "relationship",
+    "gender": "gender",
+    "residence_status": "residence_status",
+    "consent_sharing": "consent_sharing",
+}
+
+HOUSEHOLD_FIELD_MAPPINGS = {
+    "household_size": "size",
+    "village": "village",
+    "consent_sharing": "consent_sharing",
+    "country": "country",
+    "country_origin": "country_origin",
+    "head_of_household_id": "head_of_household",
+    "primary_collector_id": "primary_collector",
+    "alternate_collector_id": "alternate_collector",
+}
+
+# Administrative area field mappings
+ADMIN_AREA_MAPPINGS = {
+    "admin1": "admin1",
+    "admin2": "admin2",
+    "admin3": "admin3",
+    "admin4": "admin4",
+}
