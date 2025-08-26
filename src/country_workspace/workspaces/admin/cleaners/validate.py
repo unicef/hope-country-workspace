8e513d7d5da3a16b2e5533bd70b88c9f690efd47
data <data.mikava@gmail.com>
2025-08-26 16:45:54 +0400

    Add batch_id into the context

--- src/country_workspace/workspaces/admin/cleaners/validate.py
+++ src/country_workspace/workspaces/admin/cleaners/validate.py
@@ -3,6 +3,7 @@ from typing import Any
 
 from django.db.models import Model, QuerySet
 
+from country_workspace.context import batch_ctx
 from country_workspace.models import AsyncJob
 from country_workspace.state import state
 
@@ -17,8 +18,9 @@ def validate_queryset(queryset: QuerySet[Model], **kwargs: Any) -> dict[str, int
         program = queryset.first().program
         with state.set(tenant=program.country_office, program=program):
             for entry in queryset:
-                is_valid = entry.validate_with_checker()
-                total["valid" if is_valid else "invalid"] += 1
+                with batch_ctx(entry.batch_id):
+                    is_valid = entry.validate_with_checker()
+                    total["valid" if is_valid else "invalid"] += 1
 
     except Exception as e:  # pragma: no cover
         logger.error("Error during queryset validation: %s", e)
@@ -35,8 +37,9 @@ def validate_program(job: AsyncJob) -> dict[str, int]:
         with state.set(tenant=program.country_office, program=program):
             qs = program.households if program.beneficiary_group.master_detail else program.individuals
             for beneficiary in qs:
-                is_valid = beneficiary.validate_with_checker()
-                total["valid" if is_valid else "invalid"] += 1
+                with batch_ctx(beneficiary.batch_id):
+                    is_valid = beneficiary.validate_with_checker()
+                    total["valid" if is_valid else "invalid"] += 1
     except Exception as e:
         logger.error("Error during program validation: %s", e)
         raise
