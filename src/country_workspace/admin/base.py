e7662a2e83257b1cb5f9f406cc7e04f30dc901fe
Domenico DiNicola <dom.dinicola@gmail.com>
2025-09-02 19:04:23 +0200

    add admin actions

--- src/country_workspace/admin/base.py
+++ src/country_workspace/admin/base.py
@@ -1,7 +1,11 @@
 from admin_extra_buttons.mixins import ExtraButtonsMixin
 from adminfilters.mixin import AdminAutoCompleteSearchMixin, AdminFiltersMixin
 from django.contrib import admin
+from adminactions import actions
 
 
 class BaseModelAdmin(ExtraButtonsMixin, AdminAutoCompleteSearchMixin, AdminFiltersMixin, admin.ModelAdmin):
     pass
+
+
+actions.add_to_site(admin.site)
