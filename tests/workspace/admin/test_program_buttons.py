c18ba799f3f2d046f75091395a295fdcb1dc4f6e
data <data.mikava@gmail.com>
2025-09-09 17:13:04 +0400

    Use user_grant_permissions in unit tests

--- tests/workspace/admin/test_program_buttons.py
+++ tests/workspace/admin/test_program_buttons.py
@@ -1,13 +1,13 @@
 import pytest
 from django.contrib.admin import AdminSite
-from django.contrib.auth.models import Permission
 from django.core.exceptions import PermissionDenied
 from django.test import RequestFactory
 from django.urls import reverse
 
 from country_workspace.workspaces.admin import CountryProgramAdmin
 from country_workspace.workspaces.models import CountryProgram
-from testutils.factories import CountryProgramFactory, OfficeFactory, GroupFactory, UserFactory, UserRoleFactory
+from testutils.factories import CountryProgramFactory, OfficeFactory
+from testutils.perms import user_grant_permissions
 
 
 @pytest.fixture
@@ -31,62 +31,6 @@ def country_program(country_office):
 
 
 @pytest.fixture
-def group_with_import_permissions():
-    group = GroupFactory()
-    import_permission = Permission.objects.get(
-        codename="import_program_data",
-        content_type__app_label="country_workspace",
-    )
-    group.permissions.add(import_permission)
-    return group
-
-
-@pytest.fixture
-def user_with_import_permissions(group_with_import_permissions, country_program):
-    user = UserFactory()
-    UserRoleFactory(
-        user=user,
-        group=group_with_import_permissions,
-        program=country_program,
-        country_office=country_program.country_office,
-    )
-    return user
-
-
-@pytest.fixture
-def group_with_change_permissions():
-    group = GroupFactory()
-    change_permission = Permission.objects.get(
-        codename="change_countryprogram",
-        content_type__app_label="workspaces",
-    )
-    group.permissions.add(change_permission)
-    return group
-
-
-@pytest.fixture
-def user_with_change_permissions(group_with_change_permissions, country_program):
-    user = UserFactory()
-    UserRoleFactory(
-        user=user,
-        group=group_with_change_permissions,
-        program=country_program,
-        country_office=country_program.country_office,
-    )
-    return user
-
-
-@pytest.fixture
-def login_and_select_tenant(client):
-    def _login(user, program):
-        client.force_login(user)
-        client.post(reverse("workspace:select_tenant"), data={"tenant": program.country_office.pk})
-        return client
-
-    return _login
-
-
-@pytest.fixture
 def country_program_md_true(country_program):
     country_program.beneficiary_group.master_detail = True
     country_program.save()
@@ -104,13 +48,12 @@ def test_import_data_no_permissions(user, country_program_admin_instance, countr
 
 
 @pytest.mark.django_db  # ERA001
-def test_import_data_with_permissions(
-    user_with_import_permissions, country_program_admin_instance, country_program, client
-):
-    client.force_login(user_with_import_permissions)
-    client.post(reverse("workspace:select_tenant"), data={"tenant": country_program.country_office.pk})
-    url = reverse("workspace:workspaces_countryprogram_import_data", args=[country_program.pk])
-    response = client.get(url)
+def test_import_data_with_permissions(user, country_program_admin_instance, country_program, client):
+    with user_grant_permissions(user, "country_workspace.import_program_data", country_program):
+        client.force_login(user)
+        client.post(reverse("workspace:select_tenant"), data={"tenant": country_program.country_office.pk})
+        url = reverse("workspace:workspaces_countryprogram_import_data", args=[country_program.pk])
+        response = client.get(url)
 
     assert response.status_code == 200
 
@@ -128,12 +71,12 @@ def test_household_columns_no_permissions(user, country_program_admin_instance, 
 
 
 @pytest.mark.django_db
-def test_household_columns_with_permissions(
-    user_with_change_permissions, country_program_admin_instance, country_program_md_true, login_and_select_tenant
-):
-    client = login_and_select_tenant(user_with_change_permissions, country_program_md_true)
-    url = reverse("workspace:workspaces_countryprogram_household_columns", args=[country_program_md_true.pk])
-    response = client.get(url)
+def test_household_columns_with_permissions(user, country_program_admin_instance, country_program_md_true, client):
+    with user_grant_permissions(user, "workspaces.change_countryprogram", country_program_md_true):
+        client.force_login(user)
+        client.post(reverse("workspace:select_tenant"), data={"tenant": country_program_md_true.country_office.pk})
+        url = reverse("workspace:workspaces_countryprogram_household_columns", args=[country_program_md_true.pk])
+        response = client.get(url)
 
     assert response.status_code == 200
 
@@ -149,12 +92,12 @@ def test_individual_columns_no_permissions(user, country_program_admin_instance,
 
 
 @pytest.mark.django_db
-def test_individual_columns_with_permissions(
-    user_with_change_permissions, country_program_admin_instance, country_program, login_and_select_tenant
-):
-    client = login_and_select_tenant(user_with_change_permissions, country_program)
-    url = reverse("workspace:workspaces_countryprogram_individual_columns", args=[country_program.pk])
-    response = client.get(url)
+def test_individual_columns_with_permissions(user, country_program_admin_instance, country_program, client):
+    with user_grant_permissions(user, "workspaces.change_countryprogram", country_program):
+        client.force_login(user)
+        client.post(reverse("workspace:select_tenant"), data={"tenant": country_program.country_office.pk})
+        url = reverse("workspace:workspaces_countryprogram_individual_columns", args=[country_program.pk])
+        response = client.get(url)
 
     assert response.status_code == 200
 
@@ -170,11 +113,11 @@ def test_import_file_updates_no_permissions(user, country_program_admin_instance
 
 
 @pytest.mark.django_db
-def test_import_file_updates_with_permissions(
-    user_with_import_permissions, country_program_admin_instance, country_program, login_and_select_tenant
-):
-    client = login_and_select_tenant(user_with_import_permissions, country_program)
-    url = reverse("workspace:workspaces_countryprogram_import_file_updates", args=[country_program.pk])
-    response = client.get(url)
+def test_import_file_updates_with_permissions(user, country_program_admin_instance, country_program, client):
+    with user_grant_permissions(user, "country_workspace.import_program_data", country_program):
+        client.force_login(user)
+        client.post(reverse("workspace:select_tenant"), data={"tenant": country_program.country_office.pk})
+        url = reverse("workspace:workspaces_countryprogram_import_file_updates", args=[country_program.pk])
+        response = client.get(url)
 
     assert response.status_code == 200
