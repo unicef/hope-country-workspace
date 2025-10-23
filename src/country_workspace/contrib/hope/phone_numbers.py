375bdb9d6fbd2d43a7ee88eb98454da3353400e2
patsatsia <giorgi.patsatsia@valor-software.com>
2025-10-23 16:51:17 +0400

    Alter "PhoneNumberField"
    Change clean method and add validation

--- src/country_workspace/contrib/hope/phone_numbers.py
+++ src/country_workspace/contrib/hope/phone_numbers.py
@@ -1,13 +1,19 @@
 from django import forms
 
-from country_workspace.validators.phone_number import is_right_phone_number_format
+from country_workspace.validators.phone_number import is_right_phone_number_format, is_valid_phone_number
 
 
 class PhoneNumberField(forms.CharField):
     def clean(self, value: str) -> str:
         value = super().clean(value)
 
-        if value and not is_right_phone_number_format(value):
-            raise forms.ValidationError("Invalid phone number")
+        if value:
+            is_right, formatted_number = is_right_phone_number_format(value)
+            if not is_right:
+                raise forms.ValidationError("Invalid phone number format.")
+            value = formatted_number
+
+            if not is_valid_phone_number(value):
+                raise forms.ValidationError("Invalid phone number.")
 
         return value
