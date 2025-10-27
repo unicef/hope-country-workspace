a2585b4f51b15aebdc228d8aa155f19fd282feb9
patsatsia <giorgi.patsatsia@valor-software.com>
2025-10-27 20:19:39 +0400

    Alter logic for "DataChecker" signals
    Listen to post_delete signal as well
    
    Refactor logic

--- src/country_workspace/signals.py
+++ src/country_workspace/signals.py
@@ -1,44 +1,64 @@
-from typing import Any, TYPE_CHECKING
+from typing import Any
 
-from django.apps import apps
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from hope_flex_fields.models import Fieldset, DataCheckerFieldset, DataChecker
 
-from country_workspace.models import Program
+from country_workspace.contrib.hope.constants import (
+    HOUSEHOLD_CHECKER_NAME,
+    INDIVIDUAL_CHECKER_NAME,
+    PEOPLE_CHECKER_NAME,
+)
+from country_workspace.models import Household, Individual
 
-if TYPE_CHECKING:
-    DataChecker = apps.get_model("hope_flex_fields", "DataChecker")
+
+def _get_filtering_params(dc: DataChecker, checker: str) -> dict[str, Any]:
+    return {
+        f"batch__program__{checker}_checker": dc,
+        "removed": False,  # ignore records already pushed to hope
+        "errors": {},  # ignore already invalid records
+    }
 
 
-def _process_datachecker_change(dc: "DataChecker") -> None:
-    dc_programs = Program.objects.filter(household_checker=dc)  # adjust to individuals
-    # change queryset to fetch only programs which have households/individuals
+def _get_qs_by_dc(dc: DataChecker) -> Any:
+    if dc.name == HOUSEHOLD_CHECKER_NAME:
+        return Household.objects.filter(**_get_filtering_params(dc, "household"))
+    if dc.name in (INDIVIDUAL_CHECKER_NAME, PEOPLE_CHECKER_NAME):
+        return Individual.objects.filter(**_get_filtering_params(dc, "individual"))
 
-    for program in dc_programs:
-        households = program.households.all()
-        if households:
-            households.filter(
-                removed=False  # filters out instances already pushed to HOPE
-            ).update(errors={"data_checker": "Invalidated due to DataChecker change."})
+    return None
+
+
+def _process_datachecker_change(dc: DataChecker) -> None:
+    if not (qs := _get_qs_by_dc(dc=dc)):
+        return
+
+    batch_size = 500
+    for _start in range(0, qs.count(), batch_size):
+        qs.update(errors={"data_checker": "Invalidated due to DataChecker change."}, last_checked=None)
+
+    return
 
 
 def connect_data_checker_related_signals() -> None:
-    Fieldset = apps.get_model("hope_flex_fields", "Fieldset")
-    DataCheckerFieldset = apps.get_model("hope_flex_fields", "DataCheckerFieldset")
-
-    @receiver([post_save], sender=Fieldset)  # post/pre delete?
+    @receiver([post_save], sender=Fieldset)
     def on_fieldset_change(sender: Fieldset, instance: Fieldset, **kwargs: Any) -> None:
         if kwargs.get("created", False):
             return
 
-        dcs = instance.datachecker_set.all()
+        dcs = instance.datachecker_set.all().distinct()
         for dc in dcs:
             _process_datachecker_change(dc=dc)
 
-    @receiver([post_save], sender=DataCheckerFieldset)  # post/pre delete?
+    @receiver([post_save], sender=DataCheckerFieldset)
     def on_through_model_change(sender: DataCheckerFieldset, instance: DataCheckerFieldset, **kwargs: Any) -> None:
         if kwargs.get("created", False):
             return
 
         dc = instance.checker
         _process_datachecker_change(dc=dc)
+
+    @receiver([post_delete], sender=DataCheckerFieldset)
+    def on_through_model_deletion(sender: DataCheckerFieldset, instance: DataCheckerFieldset, **kwargs: Any) -> None:
+        # Fieldset deletion has cascading effect on DataCheckerFieldset
+        _process_datachecker_change(instance.checker)
