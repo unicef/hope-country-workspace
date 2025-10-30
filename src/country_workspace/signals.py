1f9beb717a8cb1a8c40f71d794eee01a3f0d42ec
patsatsia <giorgi.patsatsia@valor-software.com>
2025-10-30 18:38:39 +0400

    Alter "invalidate_entities_on_datachecker_change" receiver function
    Invalidate entities if program's individual/household checker is changed. Also, if beneficiary validator is being updated

--- src/country_workspace/signals.py
+++ src/country_workspace/signals.py
@@ -1,11 +1,12 @@
 from typing import Any
 
 from django.db.models import Q
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from hope_flex_fields.models import Fieldset, DataCheckerFieldset, DataChecker
 
 from country_workspace.models import Program
+from country_workspace.workspaces.models import CountryProgram
 
 
 def _get_dc_associated_programs(dc: DataChecker) -> Any:
@@ -24,13 +25,17 @@ def _invalidate_qs(qs: Any) -> None:
         )
 
 
+def _process_program(program: Program) -> None:
+    _invalidate_qs(qs=program.households.filter(removed=False))
+    _invalidate_qs(qs=program.individuals.filter(removed=False))
+
+
 def _process_datachecker_change(dc: DataChecker) -> None:
     if not (programs := _get_dc_associated_programs(dc=dc)):
         return
 
     for program in programs:
-        _invalidate_qs(qs=program.households.filter(removed=False))
-        _invalidate_qs(qs=program.individuals.filter(removed=False))
+        _process_program(program=program)
 
     return
 
@@ -38,9 +43,11 @@ def _process_datachecker_change(dc: DataChecker) -> None:
 @receiver(post_save, sender=Fieldset, dispatch_uid="cw_on_fieldset_change")
 @receiver(post_save, sender=DataCheckerFieldset, dispatch_uid="cw_on_dcfieldset_change")
 @receiver(post_delete, sender=DataCheckerFieldset, dispatch_uid="cw_on_dcfieldset_delete")
-def invalidate_entities(
-    sender: type[Fieldset | DataCheckerFieldset],
-    instance: Fieldset | DataCheckerFieldset,
+@receiver(pre_save, sender=Program, dispatch_uid="cw_on_program_change")
+@receiver(pre_save, sender=CountryProgram, dispatch_uid="cw_on_program_change")
+def invalidate_entities_on_datachecker_change(
+    sender: type[Fieldset | DataCheckerFieldset | Program],
+    instance: Fieldset | DataCheckerFieldset | Program,
     created: bool | None = None,
     **kwargs: Any,
 ) -> None:
@@ -54,3 +61,18 @@ def invalidate_entities(
     elif isinstance(instance, DataCheckerFieldset):
         dc = instance.checker
         _process_datachecker_change(dc=dc)
+
+    elif isinstance(instance, Program):
+        if not instance.pk:
+            return
+
+        old_instance = Program.objects.filter(pk=instance.pk).first()
+        if not old_instance:
+            return
+
+        if (
+            old_instance.household_checker.pk != instance.household_checker.pk
+            or old_instance.individual_checker.pk != instance.individual_checker.pk
+            or old_instance.beneficiary_validator.__class__ is not instance.beneficiary_validator.__class__
+        ):
+            _process_program(program=instance)
