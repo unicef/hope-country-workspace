31f90a6ff57b969701a61fe4d2dae432a6408baf
Giorgi Patsatsia <61111267+patsatsia@users.noreply.github.com>
2025-11-07 00:05:18 +0400

    Invalidate records on datachecker changes (#246)
    * Add a new module "signals.py"
    
    Register receiver functions for changes applied to "Fieldset" and "DataCheckerFieldset" models
    
    * Alter "ready" method of "HCWConfig" model
    
    Call "connect_data_checker_related_signals" function
    
    * Alter logic for "DataChecker" signals
    
    Listen to post_delete signal as well
    
    Refactor logic
    
    * Add test cases to cover invalidation logic via signals
    
    * Minor logic improvements
    
    * Alter import statements and fix linting issue
    
    * Make logic improvements
    
    Alter tests accordingly
    
    * Declare only one receiver function and use isinstance method to get the target datachecker
    
    Alter tests accordingly
    
    * Alter "invalidate_entities_on_datachecker_change" receiver function
    
    Invalidate entities if program's individual/household checker is changed. Also, if beneficiary validator is being updated
    
    * Add tests for invalidations during program updates
    
    * Minor adjustment
    
    Change "dispatch_uid"
    
    * Remove post_delete signal from "invalidate_entities_on_datachecker_change" receiver
    
    Alter tests accordingly
    
    * Fix the linter errors
    
    * Alter "on_delete" attribute for "household_checker" and "individual_checker" fields of "Program" model and set it to "PROTECT"
    
    Add respective migration file
    
    * Fix the linter errors
    
    * Generate a new uv.lock file
    
    ---------
    
    Co-authored-by: patsatsia <giorgi.patsatsia@valor-software.com>
    Co-authored-by: Domenico <ddinicola@unicef.org>

---
+++ src/country_workspace/signals.py
@@ -0,0 +1,75 @@
+from typing import Any
+
+from django.db.models import Q
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from hope_flex_fields.models import Fieldset, DataCheckerFieldset, DataChecker
+
+from country_workspace.models import Program
+from country_workspace.workspaces.models import CountryProgram
+
+
+def _get_dc_associated_programs(dc: DataChecker) -> Any:
+    return Program.objects.filter(Q(household_checker=dc) | Q(individual_checker=dc))
+
+
+def _invalidate_qs(qs: Any) -> None:
+    batch_size = 500
+    pks = list(qs.values_list("pk", flat=True))
+
+    for start in range(0, len(pks), batch_size):
+        batch_pks = pks[start : start + batch_size]
+        qs.filter(pk__in=batch_pks).update(
+            errors={"data_checker": "Invalidated due to DataChecker change."},
+            last_checked=None,
+        )
+
+
+def _process_program(program: Program) -> None:
+    _invalidate_qs(qs=program.households.filter(removed=False))
+    _invalidate_qs(qs=program.individuals.filter(removed=False))
+
+
+def _process_datachecker_change(dc: DataChecker) -> None:
+    if not (programs := _get_dc_associated_programs(dc=dc)):
+        return
+
+    for program in programs:
+        _process_program(program=program)
+
+    return
+
+
+@receiver(post_save, sender=Fieldset, dispatch_uid="cw_on_fieldset_change")
+@receiver(post_save, sender=DataCheckerFieldset, dispatch_uid="cw_on_dcfieldset_change")
+@receiver(pre_save, sender=Program, dispatch_uid="cw_on_program_change")
+@receiver(pre_save, sender=CountryProgram, dispatch_uid="cw_on_country_program_change")
+def invalidate_entities_on_datachecker_change(
+    sender: type[Fieldset | DataCheckerFieldset | Program],
+    instance: Fieldset | DataCheckerFieldset | Program,
+    created: bool | None = None,
+    **kwargs: Any,
+) -> None:
+    if created:
+        return
+
+    if isinstance(instance, Fieldset):
+        dcs = instance.datachecker_set.all().distinct()
+        for dc in dcs:
+            _process_datachecker_change(dc=dc)
+    elif isinstance(instance, DataCheckerFieldset):
+        dc = instance.checker
+        _process_datachecker_change(dc=dc)
+
+    elif isinstance(instance, Program):
+        if not instance.pk:
+            return
+
+        old_instance = Program.objects.filter(pk=instance.pk).first()
+
+        if (
+            old_instance.household_checker.pk != instance.household_checker.pk
+            or old_instance.individual_checker.pk != instance.individual_checker.pk
+            or old_instance.beneficiary_validator.__class__ is not instance.beneficiary_validator.__class__
+        ):
+            _process_program(program=instance)
