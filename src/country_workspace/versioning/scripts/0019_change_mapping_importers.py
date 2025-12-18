8963147577566a71f7b55495519f69aad0ec1ee7
Domenico DiNicola <dom.dinicola@gmail.com>
2025-12-18 05:22:27 -0600

    fix migrations

--- src/country_workspace/versioning/scripts/0019_change_mapping_importers.py
+++ src/country_workspace/versioning/scripts/0019_change_mapping_importers.py
@@ -1,11 +1,8 @@
 from packaging.version import Version
 from django.db import transaction
-from hope_flex_fields.models import DataChecker
-from country_workspace.models import MappingImporter
 from country_workspace.contrib.hope.constants import (
     INDIVIDUAL_CHECKER_NAME,
     PEOPLE_CHECKER_NAME,
-    HOUSEHOLD_CHECKER_NAME,
 )
 
 _script_for_version = Version("0.1.0")
@@ -40,38 +37,12 @@ def merge_rules(text: str, extra: tuple[str, ...] | list[str]) -> str:
 
 @transaction.atomic
 def forward() -> None:
-    for dc, new in RENAME.items():
-        MappingImporter.objects.filter(data_checker__name=dc).exclude(name=new).update(name=new)
-
-    if m := MappingImporter.objects.filter(data_checker__name=INDIVIDUAL_CHECKER_NAME).first():
-        want = merge_rules(m.rules, IND_RULES_ADD)
-        if want != m.rules:
-            m.rules = want
-            m.save(update_fields=["rules"])
-
-    if dc := DataChecker.objects.filter(name=HOUSEHOLD_CHECKER_NAME).first():
-        mi, _ = MappingImporter.objects.get_or_create(
-            data_checker=dc, defaults={"name": "hh_rules", "rules": "\n".join(HH_RULES)}
-        )
-        want = merge_rules(mi.rules, HH_RULES)
-        if mi.name != "hh_rules" or mi.rules != want:
-            mi.name, mi.rules = "hh_rules", want
-            mi.save(update_fields=["name", "rules"])
+    pass
 
 
 @transaction.atomic
 def backward() -> None:
-    for dc in RENAME:
-        MappingImporter.objects.filter(data_checker__name=dc).exclude(name=OLD).update(name=OLD)
-
-    if m := MappingImporter.objects.filter(data_checker__name=INDIVIDUAL_CHECKER_NAME).first():
-        cur = _lines(m.rules)
-        new = [x for x in cur if x not in IND_RULES_ADD]
-        if new != cur:
-            m.rules = "\n".join(new)
-            m.save(update_fields=["rules"])
-
-    MappingImporter.objects.filter(data_checker__name=HOUSEHOLD_CHECKER_NAME, name="hh_rules").delete()
+    pass
 
 
 class Scripts:
