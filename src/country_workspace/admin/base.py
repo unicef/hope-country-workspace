826b82e3d767ff430458ce8a6ea5c83eeb95b200
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2025-12-24 05:56:12 -0300

    add styles for adminextrabuttons select

--- src/country_workspace/admin/base.py
+++ src/country_workspace/admin/base.py
@@ -1,11 +1,23 @@
 from admin_extra_buttons.mixins import ExtraButtonsMixin
 from adminfilters.mixin import AdminAutoCompleteSearchMixin, AdminFiltersMixin
 from django.contrib import admin
+from django import forms
+
 from adminactions import actions
 
 
 class BaseModelAdmin(ExtraButtonsMixin, AdminAutoCompleteSearchMixin, AdminFiltersMixin, admin.ModelAdmin):
-    pass
+    @property
+    def media(self) -> forms.Media:
+        base = super().media
+        return base + forms.Media(
+            js=[],
+            css={
+                "screen": [
+                    "admin/admin_extra.css",
+                ],
+            },
+        )
 
 
 actions.add_to_site(admin.site)
