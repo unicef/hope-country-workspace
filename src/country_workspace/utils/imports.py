72c1826db15a919172b0877fa6e951328d46327c
patsatsia <giorgi.patsatsia@valor-software.com>
2025-12-24 12:49:10 +0400

    Alter logic for "validate_alien_fields" function

--- src/country_workspace/utils/imports.py
+++ src/country_workspace/utils/imports.py
@@ -1,23 +1,30 @@
-from typing import TYPE_CHECKING
+from country_workspace.models import Household
+from country_workspace.models import Individual
 
-from hope_flex_fields.models import DataChecker
+def validate_alien_fields(instance: Household | Individual) -> None:
+    if isinstance(instance, Household):
+        dc = instance.batch.program.household_checker
+        fields_to_ignore = instance.batch.program.hh_alien_columns_to_ignore
+    elif isinstance(instance, Individual):
+        dc = instance.batch.program.individual_checker
+        fields_to_ignore = instance.batch.program.ind_alien_columns_to_ignore
+    else:
+        dc = fields_to_ignore =None
 
-from country_workspace.utils.fields import clean_field_names
-
-if TYPE_CHECKING:
-    from country_workspace.datasources.rdi.config import Sheet
-
-
-def validate_alien_fields(sheet: "Sheet", datachecker: DataChecker) -> None:
-    if datachecker is None:
+    if dc is None:
         return
 
-    row = next(sheet)
-    raw_data = clean_field_names(row)
-    fields = set(raw_data.keys())
-    if mapping_importers := datachecker.mapping_importers.all():
+    fields = instance.flex_fields.keys()
+    if mapping_importers := dc.mapping_importers.all():
         fields = {mapping_importer.rules_as_dict.get(f, f) for f in fields for mapping_importer in mapping_importers}
 
-    dc_fields = {f"{fieldset.prefix}{field.name}" for fieldset, field in list(datachecker.get_fields())}
-    if not fields.issubset(dc_fields):
-        raise ValueError(f"Alien values found for: {fields - dc_fields}")
+    dc_fields = {f"{fieldset.prefix}{field.name}" for fieldset, field in list(dc.get_fields())}
+
+    alien_fields = fields - dc_fields
+
+    if fields_to_ignore:
+        ignored = {f.strip() for f in fields_to_ignore.split("\n") if f.strip()}
+        alien_fields = alien_fields - ignored
+
+    if alien_fields:
+        raise ValueError(f"Alien values found for: {alien_fields}")
