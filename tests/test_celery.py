57f74dcb76d490b883705a891c85f18fcab12929
patsatsia <giorgi.patsatsia@valor-software.com>
2026-01-16 13:45:15 +0400

    Add tests to cover "clean_program_data" function

--- tests/test_celery.py
+++ tests/test_celery.py
@@ -1,5 +1,9 @@
+import pytest
+
 from country_workspace.config.celery import app, init_sentry
-from country_workspace.tasks import removed_expired_jobs
+from country_workspace.tasks import removed_expired_jobs, clean_program_data
+from tests.extras.testutils.factories import ProgramFactory, BatchFactory, HouseholdFactory, IndividualFactory, \
+    AsyncJobFactory
 
 
 def test_celery_app(**kwargs):
@@ -14,3 +18,54 @@ def test_celery_init_sentry(**kwargs):
 
 def test_removed_expired_jobs(**kwargs):
     removed_expired_jobs()
+
+
+@pytest.fixture
+def program():
+    return ProgramFactory.create()
+
+
+@pytest.fixture
+def batch(program):
+    return BatchFactory.create(program=program)
+
+
+@pytest.fixture
+def job(program):
+    return AsyncJobFactory.create(program=program)
+
+
+@pytest.fixture
+def households(program, batch):
+    HouseholdFactory.create_batch(10, individuals=[], batch=batch, errors={}, removed=False)
+    HouseholdFactory.create_batch(10, individuals=[], batch=batch, removed=True)
+
+
+@pytest.fixture
+def individuals(program, batch):
+    hh = HouseholdFactory.create(batch=batch, individuals=[])
+    IndividualFactory.create_batch(10, household=hh, errors={}, removed=False)
+    IndividualFactory.create_batch(10, household=hh, removed=True)
+
+
+@pytest.mark.django_db
+def test_clean_program_data(job, batch, households, individuals):
+    assert HouseholdFactory._meta.model.objects.filter(batch=batch, removed=False).count() == 11
+    assert IndividualFactory._meta.model.objects.filter(batch=batch, removed=False).count() == 10
+
+    result = clean_program_data(job, batch_size=5)
+
+    assert result == {"individuals": 10, "households": 11}
+
+    assert HouseholdFactory._meta.model.objects.filter(batch=batch, removed=False).count() == 0
+    assert IndividualFactory._meta.model.objects.filter(batch=batch, removed=False).count() == 0
+
+    assert HouseholdFactory._meta.model.objects.filter(batch=batch).count() == 21  # 11 + 10
+    assert IndividualFactory._meta.model.objects.filter(batch=batch).count() == 20  # 10 + 10
+
+
+@pytest.mark.django_db
+def test_clean_program_data_empty_program(job):
+    result = clean_program_data(job)
+
+    assert result is None
