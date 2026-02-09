311e75ac893bc6472a62bbb3e2d58ca46a002ede
arsen-vs <arsen.pidhoretskyi@valor-software.com>
2026-02-09 14:38:02 +0200

    295713: Refactor individual and household data processing in sync.py (#323)
    * Refactor individual and household data processing in sync.py
    
    - Updated the `create_individuals` and `create_household` functions to use copies of raw data fields for processing, ensuring original data remains unchanged.
    - Changed the implementation of `default_fields_applier` to a standard function definition for clarity.
    - These adjustments enhance data integrity during the transformation and mapping processes.
    
    * Update test cases for individual and household creation in kobo_sync.py
    
    - Modified the `test_create_individuals` function to use `individual_data` for raw data, enhancing clarity and accuracy in the test.
    - Updated the `test_create_household` function to mock `extract_household_data` with a return value, ensuring that the test accurately reflects the expected data structure during household creation.
    - These changes improve the reliability of the tests by ensuring they align with the latest data processing logic.
    
    * Refactor individual data processing in sync.py
    
    - Updated the `create_individuals` function to use `raw_individual` instead of `raw_individual_fields`, enhancing clarity and ensuring the correct data structure is utilized during individual creation.
    - This change improves the integrity of the data processing logic by ensuring that only relevant fields are passed for individual creation.
    
    ---------
    
    Co-authored-by: Arsen P <arsen.pidhoretskyi@valor.com>

--- src/country_workspace/contrib/kobo/sync.py
+++ src/country_workspace/contrib/kobo/sync.py
@@ -118,7 +118,7 @@ def create_individuals(
         )
         default_fields_callable = cast("Callable[[Raw], Raw]", partial(batch.program.apply_default_fields, Individual))
         individual_fields = preprocess(
-            raw_individual,
+            raw_individual.copy(),
             INDIVIDUAL_FIELDS_TO_UPPERCASE + TO_UPPERCASE_FIELDS,
             mapping_importer_callable,
             default_fields_callable,
@@ -131,7 +131,7 @@ def create_individuals(
                 name=individual_fields.get(fullname, "") if fullname else "",
                 originating_id=originating_id,
                 flex_fields=individual_fields,
-                raw_data=individual_fields,
+                raw_data=raw_individual,
             ),
         )
     household.program.individuals.bulk_create(individuals)
@@ -159,7 +159,7 @@ def create_household(
     )
     default_fields_callable = cast("Callable[[Raw], Raw]", partial(batch.program.apply_default_fields, Household))
     household_fields = preprocess(
-        raw_household_fields,
+        raw_household_fields.copy(),
         HOUSEHOLD_FIELDS_TO_UPPERCASE,
         mapping_importer_callable,
         default_fields_callable,
@@ -171,7 +171,7 @@ def create_household(
             batch=batch,
             originating_id=originating_id,
             flex_fields=household_fields,
-            raw_data=household_fields,
+            raw_data=raw_household_fields,
         ),
     )
 
