7e31b011abaeea6f21a867df5827580ceb47aa87
Vitali Yanushchyk <vitali.yanushchyk@valor-software.com>
2026-09-15 02:02:27 -0300

    drop legacy automatic create-dedup-push flow.

--- src/country_workspace/api/callbacks/urls.py
+++ src/country_workspace/api/callbacks/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 
-from .dedup_engine.views import DeduplicationCallbackView
 from .hope.views import HopeRdpPushReadyCallbackView
 
 
 app_name = "callbacks"
 
 urlpatterns = [
-    path(
-        "dedup-engine/rdps/state-changed/<str:signed_token>/",
-        DeduplicationCallbackView.as_view(),
-        name="dedup-engine-rdp-state-changed",
-    ),
     path(
         "hope/rdps/push-ready/",
         HopeRdpPushReadyCallbackView.as_view(),
--- src/country_workspace/rdp/__init__.py
+++ src/country_workspace/rdp/__init__.py
@@ -1,10 +1,4 @@
-from .deduplication.constants import DEDUP_CALLBACK_MAX_AGE, DEDUP_CALLBACK_SALT
-from .deduplication.workflow import (
-    claim_rdp_deduplication,
-    create_and_push_rdp_core,
-    dedup_callback_handle,
-    dedup_existing_rdp_core,
-)
+from .deduplication.workflow import claim_rdp_deduplication, dedup_existing_rdp_core
 from .lifecycle import cancel_existing_rdp_core, create_rdp_core, reset_rdp
 from .policy import DedupEngineState, get_program_dedup_settings_policy, get_rdp_policy
 from .push.constants import PUSH_READY_CALLBACK_MAX_AGE, PUSH_READY_CALLBACK_SALT
@@ -12,8 +6,6 @@ from .push.workflow import claim_rdp_push, fail_stuck_rdp_push, handle_push_read
 from .types import CreateRdpConfig
 
 __all__ = [
-    "DEDUP_CALLBACK_MAX_AGE",
-    "DEDUP_CALLBACK_SALT",
     "PUSH_READY_CALLBACK_MAX_AGE",
     "PUSH_READY_CALLBACK_SALT",
     "CreateRdpConfig",
@@ -21,9 +13,7 @@ __all__ = [
     "cancel_existing_rdp_core",
     "claim_rdp_deduplication",
     "claim_rdp_push",
-    "create_and_push_rdp_core",
     "create_rdp_core",
-    "dedup_callback_handle",
     "dedup_existing_rdp_core",
     "fail_stuck_rdp_push",
     "get_program_dedup_settings_policy",
